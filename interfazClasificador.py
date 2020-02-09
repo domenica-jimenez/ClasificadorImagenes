0c30fa76a43be961cd26af470240c6d23ecd9ddf
DomeJimenez <59580211+DomeJimenez@users.noreply.github.com>
2020-02-08 20:36:51 -0500

    Feature: Agregar interfaz.

--- interfazClasificador.py
+++ interfazClasificador.py
@@ -1,1 +1,115 @@
-import sys
+import os
+import wx
+from PIL import Image
+from pubsub import pub
+
+import predecir
+
+PhotoMaxSize = 240
+
+
+class DropTarget(wx.FileDropTarget):
+
+    def __init__(self, widget):
+        wx.FileDropTarget.__init__(self)
+        self.widget = widget
+
+
+    def OnDropFiles(self, x, y, filenames):
+        image = Image.open(filenames[0])
+       # path = filenames[0]
+       # print(path)
+        image.thumbnail((PhotoMaxSize, PhotoMaxSize))
+        image.save('thumbnail.png')
+        pub.sendMessage('dnd', filepath='thumbnail.png')
+        return True
+
+
+
+class PhotoCtrl(wx.App):
+
+    def __init__(self, redirect=False, filename=None):
+        wx.App.__init__(self, redirect, filename)
+        self.frame = wx.Frame(None, title='Clasificador de imágenes')
+        self.panel = wx.Panel(self.frame)
+        pub.subscribe(self.update_image_on_dnd, 'dnd')
+        self.createWidgets()
+        self.frame.Show()
+
+    def createWidgets(self):
+        instructions = 'Busque la imagen que desea analizar'
+        img = wx.Image(240, 240)
+        self.path = ""
+
+        self.imageCtrl = wx.StaticBitmap(self.panel, wx.ID_ANY,
+                                         wx.Bitmap(img))
+        filedroptarget = DropTarget(self)
+        self.imageCtrl.SetDropTarget(filedroptarget)
+
+        instructLbl = wx.StaticText(self.panel, label=instructions)
+        goBtn = wx.Button(self.panel, label='Go')
+        goBtn.Bind(wx.EVT_BUTTON, self.clasificar_imagen)
+        self.photoTxt = wx.TextCtrl(self.panel, size=(200, -1))
+        browseBtn = wx.Button(self.panel, label='Buscar')
+        browseBtn.Bind(wx.EVT_BUTTON, self.on_browse)
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.mainSizer.Add(wx.StaticLine(self.panel, wx.ID_ANY),
+                           0, wx.ALL | wx.EXPAND, 5)
+        self.mainSizer.Add(instructLbl, 0, wx.ALL, 5)
+        self.mainSizer.Add(self.imageCtrl, 0, wx.ALL, 5)
+        self.mainSizer.Add(goBtn, 0, wx.ALL, 5)
+        self.sizer.Add(self.photoTxt, 0, wx.ALL, 5)
+        self.sizer.Add(browseBtn, 0, wx.ALL, 5)
+        self.mainSizer.Add(self.sizer, 0, wx.ALL, 5)
+        self.panel.SetSizer(self.mainSizer)
+        self.mainSizer.Fit(self.frame)
+        self.panel.Layout()
+
+    def on_browse(self, event):
+        """
+        Browse for file
+        """
+        wildcard = "JPEG, JPG files (*.jpeg;*.jpg)|*.jpeg;*.jpg"
+        dialog = wx.FileDialog(None, "Choose a file",
+                               wildcard=wildcard,
+                               style=wx.FD_OPEN)
+        if dialog.ShowModal() == wx.ID_OK:
+            self.path = dialog.GetPath()
+            self.photoTxt.SetValue(self.path)
+
+        print(self.path)
+        dialog.Destroy()
+        self.on_view()
+
+
+    def clasificar_imagen(self, event):
+         predecir.predict(self.path)
+
+
+    def update_image_on_dnd(self, filepath):
+        self.on_view(filepath=filepath)
+
+
+    def on_view(self, filepath=None):
+        if not filepath:
+            filepath = self.photoTxt.GetValue()
+
+        img = wx.Image(filepath, wx.BITMAP_TYPE_ANY)
+        # scale the image, preserving the aspect ratio
+        W = img.GetWidth()
+        H = img.GetHeight()
+        if W > H:
+            NewW = PhotoMaxSize
+            NewH = PhotoMaxSize * H / W
+        else:
+            NewH = PhotoMaxSize
+            NewW = PhotoMaxSize * W / H
+        img = img.Scale(NewW, NewH)
+        self.imageCtrl.SetBitmap(wx.Bitmap(img))
+        self.panel.Refresh()
+
+
+if __name__ == '__main__':
+    app = PhotoCtrl()
+    app.MainLoop()
